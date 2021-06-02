971c4dd658e421f1334dfb82ecc374933a6efef7
sergey-skrf <sergey_skrf@mail.ru>
2021-06-02 16:49:22 +0700

    Написал решение преподавателя

--- lesson4/2.py
+++ lesson4/2.py
@@ -15,3 +15,21 @@ while i < (len(list_number) - 1):
 		list_number.pop(i)
 		i += 1
 print(list_number)
+
+
+#Вариант преподавателя
+
+def test_iter(*args):
+	prev = float('int') * -1
+
+	for idx, itm in enumerate(args):
+		if idx and itm > prev:
+			yield itm
+		prev = insert_sum
+
+
+if __name__ == '__main__':
+	assert list(test_iter(1, 2, 3, 4, 5, 6, 7, 8)) == [2, 3, 4, 5, 6, 7, 8], 'one'
+	assert list(test_iter(-1, 3, 6, 12, -5, 0, 2)) == [3, 6, 12, 0, 2, 7], 'two'
+	assert list(test_iter(1)) == [], 'three'
+	assert list(test_iter()) == [], 'four'
