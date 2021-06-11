49624a0a26de5389cc96a1dc051330304d45f4e6
Sergey-skrf <sergey_skrf@mail.ru>
2021-06-11 22:50:24 +0700

    Устранил синтаксичесие ошибки (отступы)

--- lesson6/Lesson6.py
+++ lesson6/Lesson6.py
@@ -1,9 +1,7 @@
 import random
 class Homo:
-	
 
 	__population = 0
-
 
 	def __init__(self, height, sex, age, weight, father=None, mother=None):
 		self.height = height
@@ -17,7 +15,6 @@ class Homo:
 		Homo.__population += 1
 
 
-
 	def say(self):
 		print('HEYYYYY')
 
@@ -27,42 +24,40 @@ class Homo:
 		print(cls.__population)
 
 
-
 	def say_duble_ind(self):
 		print(self.__duble_ind)
 
 
-
 class Human(Homo):
+
 	def __init__(self, height, sex, age, weight, name, surname, father=None, mother=None):
 		self.name = name
 		self.surname = surname
 		super().__init__(height, sex, age, weight, father, mother)
 
-
 	def say2(self):
 		print(self._ind)
 
-
-    def say(self):
-    	print(f'my name is {self.name} {self.surname}')
+	def say(self):
+		print(f'my name is {self.name} {self.surname}')
 
 
 
 	def reproduction(self, mother):
-		'''
 
+		'''
 		:param mother: Human
 		:return: Human
         '''
-		return Human(height=20,
-					sex=random.choise(('w', 'm')),
-					age=0,
-					weight=3.600,
-					name=mother.name,
-					surname=self.surname, 
-					father=self, 
-					mother=mother)
+		return Human(
+			height=20,
+			sex=random.choice(('w', 'm')),
+			age=0,
+			weight=3.600,
+			name=mother.name,
+			surname=self.surname,
+			father=self,
+			mother=mother)
 
 
 
@@ -70,5 +65,6 @@ class Human(Homo):
 
 if __name__ == '__main__':
 	dasha = Human(170, 'w', 22, 55, 'Даша', 'Иванова')
+	print(1)
 	petr = Human(170, 'm', 22, 55, 'Петр', 'Иванов')
-	child = peter.reproduction(dasha)
+	child = petr.reproduction(dasha)
